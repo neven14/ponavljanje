560c623cb0796da25e93d806e3ae02a0abfe33c6
neven14 <58472164+neven14@users.noreply.github.com>
2019-12-17 14:03:31 +0100

    Add files via upload

---
+++ dijagonala kvadratnog polja.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int n;
+	cout<<"Upisi broj veci od 2 i manjeg od 100: "<<endl;
+	cin>>n;
+	
+	
+	for(int a=1;a<=n;a++)
+	 cout<<" "<<a;
+	if(int i=n)
+		cout<<" "<<i<<endl;
+		
+	
+	
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	system("pause");
+	return 0;
+}
